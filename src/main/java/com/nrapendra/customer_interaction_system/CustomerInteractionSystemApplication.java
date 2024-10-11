fa9cc4ac9c6c26c753096513334cf2a427ec1589
Nrapendra Trivedi <trivajay259@gmail.com>
2024-10-11 12:00:20 +0200

    created customer interaction system

---
+++ src/main/java/com/nrapendra/customer_interaction_system/CustomerInteractionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nrapendra.customer_interaction_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInteractionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerInteractionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nrapendra/customer_interaction_system/CustomerInteractionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrapendra.customer_interaction_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInteractionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
